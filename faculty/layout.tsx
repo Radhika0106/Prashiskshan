0e65bebca1d456b823ddce42bc3a43c85e4c61bf
Radhika0106 <2024a1r113@mietjammu.in>
2026-06-10 18:38:43 +0530

    Delete faculty directory

--- faculty/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { DashboardLayout } from "@/components/DashboardLayout";
-
-export default function FacultyLayout({ children }: { children: ReactNode }) {
-  return <DashboardLayout role="faculty">{children}</DashboardLayout>;
-}
